63b5677943bfd3a135fa5ec0ca7a61d0c032785f
alfficcadenti <alfredoficcadenti@gmail.com>
2019-01-22 02:16:54 +0000

    Nav bar and compose tweet CSS style

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+
+//JAvascript
+document.querySelector("textarea").addEventListener("focus",function() {
+  //console.log("focused")
+})
+
+//jQuery code:
+$("textarea").on("click", function() {
+  console.log("jquwery")
+})
+
+
+
+});
