3e2677d0ce68815ed1e8ad2d2f7124de3cfeb854
alfficcadenti <alfredoficcadenti@gmail.com>
2019-01-22 23:59:33 +0000

    added createTweetElement function to app.js

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -4,3 +4,49 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+// Test / driver code (temporary). Eventually will get this from the server.
+
+const tweetData = {
+  "user": {
+    "name": "Newton",
+    "avatars": {
+      "small":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_50.png",
+      "regular": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188.png",
+      "large":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_200.png"
+    },
+    "handle": "@SirIsaac"
+  },
+  "content": {
+    "text": "If I have seen further it is by standing on the shoulders of giants"
+  },
+  "created_at": 1461116232227
+}
+
+function createTweetElement(tweetData) {
+
+  let $tweet =
+          ($("<article>").addClass("tweet")
+          // append a header to the article
+          .append($("<header>")
+            .append($("<img>")
+              .attr("src",tweetData.user.avatars.small))
+            .append($("<span>").text(tweetData.user.name).addClass("username"))
+
+            .append($("<span>").text(tweetData.user.handle).addClass("handle")
+              )
+          )
+          // append a main to the article
+          .append($("<div>").text(tweetData.content.text))
+          // append a footer to the article
+          .append($("<footer>").text("data"))
+          );
+  return $tweet;
+}
+
+$(document).ready(function() {
+var $tweet = createTweetElement(tweetData);
+
+
+$('#tweet-container').append($tweet);
+
+});
--- public/scripts/load-tweets.js
+++ public/scripts/load-tweets.js
@@ -1,70 +1,70 @@
-function dateDiff(date) {
-    var second=1000, minute=second*60, hour=minute*60, day=hour*24, week=day*7, month=week*4;
-    date = new Date(date);
-    let Now = Date.now();
-    var timediff = Now - date;
-    if (isNaN(timediff)) return NaN;
-    //if less than 1 minute
-    if (timediff < 1000*60) {
-        let time = Math.floor(timediff / second);
-        return time + " seconds ago";
-    }
-    //if less than 1 hour
-    else if (timediff < 1000*60*60) {
-        let time = Math.floor(timediff / minute);
-        return time + " minutes ago";
-    }
-    //if less than 1 day
-    else if (timediff < 1000*60*60*24) {
-        let time = Math.floor(timediff / hour);
-        return time + " days ago";
-    }
-    //if less than 1 week
-    else if (timediff < 1000*60*60*24*7) {
-        let time = Math.floor(timediff / day);
-        return time + " months ago";
-    }
-    else {
-        let time = Math.floor(timediff / month);
-        return time + " months ago";
-    }
-}
+// function dateDiff(date) {
+//     var second=1000, minute=second*60, hour=minute*60, day=hour*24, week=day*7, month=week*4;
+//     date = new Date(date);
+//     let Now = Date.now();
+//     var timediff = Now - date;
+//     if (isNaN(timediff)) return NaN;
+//     //if less than 1 minute
+//     if (timediff < 1000*60) {
+//         let time = Math.floor(timediff / second);
+//         return time + " seconds ago";
+//     }
+//     //if less than 1 hour
+//     else if (timediff < 1000*60*60) {
+//         let time = Math.floor(timediff / minute);
+//         return time + " minutes ago";
+//     }
+//     //if less than 1 day
+//     else if (timediff < 1000*60*60*24) {
+//         let time = Math.floor(timediff / hour);
+//         return time + " days ago";
+//     }
+//     //if less than 1 week
+//     else if (timediff < 1000*60*60*24*7) {
+//         let time = Math.floor(timediff / day);
+//         return time + " months ago";
+//     }
+//     else {
+//         let time = Math.floor(timediff / month);
+//         return time + " months ago";
+//     }
+// }
 
 
-$(document).ready(function() {
-  //jQuery get tweets Json and appen to the main page:
+// $(document).ready(function() {
+//   //jQuery get tweets Json and appen to the main page:
 
-  //get json
-  $.get("/tweets", function(data, status){
-    //alert("Data: " + data + "\nStatus: " + status);
-    //for loop on tweet data
-    for (let tweet in data) {
-      console.log(data[tweet])
-      // pass to the build function to get HTML code
-      //let tweetHTML = buildTweet(data[tweet]);
-      let tweetObj = data[tweet];
-      //
-      // calculate date distance
-      var date = dateDiff(data[tweet].created_at);
-      console.log(date)
-      //append the article to the tweet container
-      $("#tweet-container")
-        .append($("<article>").addClass("tweet")
-          // append a header to the article
-          .append($("<header>")
-            .append($("<img>")
-              .attr("src",tweetObj.user.avatars.small))
-            .append($("<span>").text(tweetObj.user.name).addClass("username"))
+//   //get json
+//   $.get("/tweets", function(data, status){
+//     //alert("Data: " + data + "\nStatus: " + status);
+//     //for loop on tweet data
+//     for (let tweet in data) {
+//       console.log(data[tweet])
+//       // pass to the build function to get HTML code
+//       //let tweetHTML = buildTweet(data[tweet]);
+//       let tweetObj = data[tweet];
+//       //
+//       // calculate date distance
+//       var date = dateDiff(data[tweet].created_at);
+//       console.log(date)
+//       //append the article to the tweet container
+//       $("#tweet-container")
+//         .append($("<article>").addClass("tweet")
+//           // append a header to the article
+//           .append($("<header>")
+//             .append($("<img>")
+//               .attr("src",tweetObj.user.avatars.small))
+//             .append($("<span>").text(tweetObj.user.name).addClass("username"))
 
-            .append($("<span>").text(tweetObj.user.handle).addClass("handle")
-              )
-          )
-          // append a main to the article
-          .append($("<div>").text(tweetObj.content.text))
-          // append a footer to the article
-          .append($("<footer>").text(date))
-        );
-    }
-  });
+//             .append($("<span>").text(tweetObj.user.handle).addClass("handle")
+//               )
+//           )
+//           // append a main to the article
+//           .append($("<div>").text(tweetObj.content.text))
+//           // append a footer to the article
+//           .append($("<footer>").text(date))
+//         );
+//     }
+//   });
 
-});
+// });
