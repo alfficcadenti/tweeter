e85c1c925fba849553163888c380eff6ddcd3a79
alfficcadenti <alfredoficcadenti@gmail.com>
2019-01-23 03:33:12 +0000

    AJAX call to prevent form submit

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -38,54 +38,6 @@ function tweetAge(date) {
     }
 }
 
-const data = [
-  {
-    "user": {
-      "name": "Newton",
-      "avatars": {
-        "small":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_50.png",
-        "regular": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188.png",
-        "large":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_200.png"
-      },
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1461116232227
-  },
-  {
-    "user": {
-      "name": "Descartes",
-      "avatars": {
-        "small":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_50.png",
-        "regular": "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc.png",
-        "large":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_200.png"
-      },
-      "handle": "@rd" },
-    "content": {
-      "text": "Je pense , donc je suis"
-    },
-    "created_at": 1461113959088
-  },
-  {
-    "user": {
-      "name": "Johann von Goethe",
-      "avatars": {
-        "small":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_50.png",
-        "regular": "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1.png",
-        "large":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_200.png"
-      },
-      "handle": "@johann49"
-    },
-    "content": {
-      "text": "Es ist nichts schrecklicher als eine tätige Unwissenheit."
-    },
-    "created_at": 1461113796368
-  }
-];
-
-
 function renderTweets(tweets) {
   for (let i in tweets) {
       let $tweet = createTweetElement(tweets[i]);
@@ -117,6 +69,28 @@ function createTweetElement(tweetData) {
 
 $(document).ready(function() {
 
-  renderTweets(data);
+  //GET TWEETS
+  $.get("/tweets", function(data, status){
+    renderTweets(data);
+  });
+
+
+  //PREVENT FORM SUBMISSION AND AJAX CALL
+  $("section.new-tweet > form").submit(function( event ) {
+    let content = ""//this.serialize();
+    event.preventDefault();
+    //get the action-url of the form
+    var actionurl = event.currentTarget.action;
+    //do your own request an handle the results
+    $.ajax({
+      url: actionurl,
+      type: 'post',
+      dataType: 'application/json',
+      data: $("section.new-tweet > form").serialize(),
+      success: function(data) {console.log(data)}
+    });
+  });
 
 });
+
+
